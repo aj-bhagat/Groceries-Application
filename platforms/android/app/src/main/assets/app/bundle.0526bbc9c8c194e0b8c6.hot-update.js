75e944e79e87bbc3df01f7d21078bfae08d6950e
aj-bhagat <ajaybhagatm620@gmail.com>
2019-09-18 18:21:10 +0530

    Login Page

---
+++ platforms/android/app/src/main/assets/app/bundle.0526bbc9c8c194e0b8c6.hot-update.js
@@ -0,0 +1,32 @@
+webpackHotUpdate("bundle",{
+
+/***/ "./app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("../node_modules/@angular/core/fesm5/core.js");
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+    }
+    AppComponent.prototype.submit = function () {
+        console.log("hello");
+    };
+    AppComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: "gr-login",
+            template: __webpack_require__("./pages/login/login.html"),
+            styles: [__webpack_require__("./pages/login/login.android.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAuY29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBQUE7QUFBQTtBQUEwQztBQVExQztJQUFBO0lBSUEsQ0FBQztJQUhDLDZCQUFNLEdBQU47UUFDRSxPQUFPLENBQUMsR0FBRyxDQUFDLE9BQU8sQ0FBQyxDQUFDO0lBQ3ZCLENBQUM7SUFIVSxZQUFZO1FBTnhCLCtEQUFTLENBQUM7WUFDVCxRQUFRLEVBQUUsVUFBVTtZQUVwQix5REFBdUM7O1NBRXhDLENBQUM7T0FDVyxZQUFZLENBSXhCO0lBQUQsbUJBQUM7Q0FBQTtBQUp3QiIsImZpbGUiOiJidW5kbGUuMDUyNmJiYzljOGMxOTRlMGI4YzYuaG90LXVwZGF0ZS5qcyIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IENvbXBvbmVudCB9IGZyb20gXCJAYW5ndWxhci9jb3JlXCI7XG5cbkBDb21wb25lbnQoe1xuICBzZWxlY3RvcjogXCJnci1sb2dpblwiLFxuICBtb2R1bGVJZDogbW9kdWxlLmlkLFxuICB0ZW1wbGF0ZVVybDogXCIuL3BhZ2VzL2xvZ2luL2xvZ2luLmh0bWxcIixcbiAgc3R5bGVVcmxzOiBbXCIuL3BhZ2VzL2xvZ2luL2xvZ2luLmFuZHJvaWQuY3NzXCJdXG59KVxuZXhwb3J0IGNsYXNzIEFwcENvbXBvbmVudCB7XG4gIHN1Ym1pdCgpe1xuICAgIGNvbnNvbGUubG9nKFwiaGVsbG9cIik7XG4gIH1cbn0iXSwic291cmNlUm9vdCI6IiJ9
